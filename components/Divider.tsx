42c54213e7e41e7c5401b131701bad8097409fe8
Dein Name <deine@email.de>
2026-07-24 00:47:50 +0200

    Kapitelbilder mit weichen Rändern versehen

---
+++ components/Divider.tsx
@@ -0,0 +1,11 @@
+export default function Divider() {
+    return (
+
+        <div className="py-20 bg-[#090909]">
+
+            <div className="mx-auto h-px w-40 bg-[#d8b16d]/40" />
+
+        </div>
+
+    );
+}
